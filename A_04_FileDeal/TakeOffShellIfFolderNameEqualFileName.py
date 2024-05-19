ff14924c8ac32077275b9b699b17cc46ee77f269
wr <wr@qq.com>
2024-05-19 11:12:00 +0800

    tijiao

--- A_04_FileDeal/TakeOffShellIfFolderNameEqualFileName.py
+++ A_04_FileDeal/TakeOffShellIfFolderNameEqualFileName.py
@@ -8,28 +8,43 @@ def take_off_shell_if_folder_name_equal_file_name(_root_path):
     list_folder_name_equal_file_name = []
     for sub_item in os.listdir(_root_path):
         sub_item_path = os.path.join(_root_path, sub_item)
-        if not os.path.isdir(sub_item_path):
-            continue
-        sub_item_content_list = os.listdir(sub_item_path)
-        if len(sub_item_content_list) == 1:
-            if sub_item_content_list[0] == sub_item:
+        if os.path.isdir(sub_item_path) and '.' in sub_item:
+            if len(os.listdir(sub_item_path)) == 1 and os.listdir(sub_item_path)[0] == sub_item:
                 if os.path.isfile(os.path.join(sub_item_path, sub_item)):
                     list_folder_name_equal_file_name.append(sub_item)
     print(f'扫描到{len(list_folder_name_equal_file_name)}个文件夹名与内部文件名相同的文件夹{list_folder_name_equal_file_name}')
+
+    move_them = ''
     for dir_matched in list_folder_name_equal_file_name:
         print(f'满足条件的文件夹: {dir_matched}')
-
-        # token_off_shell_files_folder = os.path.join(_root_path, 'TokenOffShellFiles')
-        # if not os.path.isdir(token_off_shell_files_folder):
-        #     os.makedirs(token_off_shell_files_folder)
-        # empty_shells_folder = os.path.join(_root_path, 'EmptyShells')
-        # if not os.path.isdir(empty_shells_folder):
-        #     os.makedirs(empty_shells_folder)
-        # path_dir_matched = os.path.join(_root_path, dir_matched)
-        # path_file_matched = os.path.join(path_dir_matched, dir_matched)
-        # shutil.move(path_file_matched, token_off_shell_files_folder)
-        # shutil.move(path_dir_matched, empty_shells_folder)
-
+        if not move_them:
+            move_them = input(f'''要不要剪切呢? 
+            n=不剪切
+            N=不剪切不再提示
+            y=剪切
+            Y=剪切不再提示
+            请输入:''')
+        if move_them in ['N', 'n']:
+            if move_them == 'n':
+                move_them = ''
+            continue
+        elif move_them in ['Y', 'y']:
+            if move_them == 'y':
+                move_them = ''
+            token_off_shell_files_folder = os.path.join(_root_path, '00.TokenOffShellFiles')
+            if not os.path.isdir(token_off_shell_files_folder):
+                os.makedirs(token_off_shell_files_folder)
+            empty_shells_folder = os.path.join(_root_path, '00.EmptyShells')
+            if not os.path.isdir(empty_shells_folder):
+                os.makedirs(empty_shells_folder)
+            path_dir_matched = os.path.join(_root_path, dir_matched)
+            path_file_matched = os.path.join(path_dir_matched, dir_matched)
+            shutil.move(path_file_matched, token_off_shell_files_folder)
+            shutil.move(path_dir_matched, empty_shells_folder)
+            print(f'脱壳完成: {dir_matched}')
+        else:
+            if move_them == 'n':
+                move_them = ''
 
 
 if __name__ == '__main__':
